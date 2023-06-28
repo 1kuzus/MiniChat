02abb89ec32481b25939f855d0d3636a48b4c1f9
1kuzus <iku2u5@outlook.com>
2023-06-29 00:16:42 +0800

    0629

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import OpenConversation from './OpenConversation'
+import {useConversations} from '../contexts/ConversationsProvider'
+
 
 export default function Dashboard(props)
 {
     const {id}=props
+    const {selectedConversation}=useConversations()
 
     return (
-        <Sidebar id={id}/>
+        <div style={{display:'flex',height:'100vh'}}>
+            <Sidebar id={id}/>
+            <OpenConversation/>
+        </div>
     )
 }
--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './OpenConversation.css'
 
 export default function OpenConversation()
 {
     return (
-        <div>OpenConversation</div>
+        <div className='open-conversation-container'>
+            <div className='messages'>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+                <div style={{height:'200px'}}>123</div>
+            </div>
+        </div>
     )
 }
